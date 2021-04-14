c88d1a5b38415414fc2db84c26b59b847446fd7b
gonzalo <gonzaloytorres@gmail.com>
2021-04-14 13:36:34 +0200

    Arreglo tipos funciones

---
+++ src/laberinto.hpp
@@ -0,0 +1,11 @@
+
+class laberinto{
+    private:
+    int x;
+    int y;
+    
+    public:
+    //aquí iria el constructor que no me acuerdo como se hace
+    setPosicion(int, int); //metodo que modifica los valores x e y
+    siguientePosicion(int, int); //metodo que adelanta un movimiento 
+};
