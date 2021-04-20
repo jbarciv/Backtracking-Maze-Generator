c013b0d596c8472ea67f0979ec13bfe00cec679d
gonzalo <gonzaloytorres@gmail.com>
2021-04-20 13:25:55 +0200

    Retoques en metodos y encabezado

--- src/Iterativo.cpp
+++ src/Iterativo.cpp
@@ -1,11 +1,13 @@
 #include "laberinto.hpp"
 
 int main(){
-   unsigned int i;
-   int filas=0;
-   int columnas=0;
+    unsigned int i;
+    int filas=0;
+    int columnas=0;
 
-   // Se pide al usuario el tamaño del laberinto.
+    printf("Iterativoo");
+
+    // Se pide al usuario el tamaño del laberinto.
     printf("Introduzca dimensiones\n Filas: ");
     scanf("%u", &filas);
     printf(" Columnas: ");
@@ -13,34 +15,36 @@ int main(){
 
     columnas+=2;   
     filas+=2; // Añadimos un marco que serán los bordes del laberinto
-    
+
     char **grid = NULL;
     // Se reserva memoria para el vector de vectores dinámicos (del tamaño de "filas").
-  	grid = (char **) malloc (filas  * sizeof(char *)); 
-	if (grid == NULL) {
-  		printf("No se pudo reservar memoria\n");
-		return -1;
-  	}
+    grid = (char **) malloc (filas  * sizeof(char *)); 
+    if (grid == NULL) {
+        printf("No se pudo reservar memoria\n");
+        return -1;
+    }
     // Se reserva memoria para cada fila (del tamaño de "columnas").
-  	for (i = 0; i < filas; ++i) {
-    	grid[i] = (char *) malloc (columnas * sizeof(char));
-    	if (grid[i] == NULL) {
-  			printf("No se pudo reservar memoria\n");
-			return -1;
-  		}
-  	}
-    
+    for (i = 0; i < filas; ++i) {
+        grid[i] = (char *) malloc (columnas * sizeof(char));
+        if (grid[i] == NULL) {
+            printf("No se pudo reservar memoria\n");
+            return -1;
+        }
+    }
+
     srand(time(0)); // Generador de numero aleatorio.
 
     SetGrid(grid, filas, columnas);
     MakeMaze(grid);
     PrintGrid(grid,filas,columnas);
-    
+
     // Se libera memoria para cada fila.
     for (i = 0; i < filas; ++i) {
-    	free(grid[i]);
+        free(grid[i]);
     }
-  	free(grid); //Se libera memoria para el vector de vectores ("primera columna").
+    free(grid); //Se libera memoria para el vector de vectores ("primera columna").
+
+    system("pause");
 
     return 0;
 }
--- src/laberinto.hpp
+++ src/laberinto.hpp
@@ -7,9 +7,9 @@
 #define ABAJO 2
 #define IZQUIERDA 3
 
-bool comprobacion (int, int, int, char**){}
-bool esPosible (int, int, char**){}
-bool isNextPossible(int, int, char**){}
-void SetGrid(char**, int, int){}
-void PrintGrid(char **, int, int){}
-void MakeMaze (char **){}
+bool comprobacion (int, int, int, char**);
+bool esPosible (int, int, char**);
+bool isNextPossible(int, int, char**);
+void SetGrid(char**, int, int);
+void PrintGrid(char **, int, int);
+void MakeMaze (char **);
