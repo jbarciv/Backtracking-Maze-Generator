73246e44262f3b6016102f5c24247236b332e2ee
Josep <chemabc@gmail.com>
2021-04-25 12:49:15 +0200

    más cambios de carpetas

---
+++ src/Otros/Iterativo.cpp
@@ -0,0 +1,53 @@
+#include "laberinto.hpp"
+
+int main(){
+    int i;// chema: he quitado el unsigned
+    int filas=0;
+    int columnas=0;
+
+    printf("Iterativo");
+
+    // Se pide al usuario el tamaño del laberinto.
+    printf("Introduzca dimensiones\n Filas: ");
+    scanf("%d", &filas);
+    printf(" Columnas: ");
+    scanf("%d", &columnas); // he puesto %d... por ser enteros (chema)
+
+    columnas+=2;   // (chema): creo que no tiene por qué hacerse...
+    filas+=2; // Añadimos un marco que serán los bordes del laberinto
+
+    char **grid = NULL;
+    // Se reserva memoria para el vector de vectores dinámicos (del tamaño de "filas").
+    grid = (char **) malloc (filas  * sizeof(char *)); 
+    if (grid == NULL) {
+        printf("No se pudo reservar memoria\n");
+        return -1;
+    }
+    // Se reserva memoria para cada fila (del tamaño de "columnas").
+    for (i = 0; i < filas; ++i) {
+        grid[i] = (char *) malloc (columnas * sizeof(char));
+        if (grid[i] == NULL) {
+            printf("No se pudo reservar memoria\n");
+            return -1;
+        }
+    }
+
+    srand(time(0)); // Generador de numero aleatorio.
+    printf("he llegado hasta aquí\n");
+    SetGrid(grid, filas, columnas);
+    printf("he llegado hasta aquí\n");
+    MakeMaze(grid);
+    printf("he llegado hasta aquí\n"); 
+    PrintGrid(grid,filas,columnas);
+
+    // Se libera memoria para cada fila.
+    for (i = 0; i < filas; ++i) {
+        free(grid[i]);
+    }
+    free(grid); //Se libera memoria para el vector de vectores ("primera columna").
+
+    system("pause");
+
+    return 0;
+}
+
