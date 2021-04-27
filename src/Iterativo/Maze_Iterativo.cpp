582287601170936d1f61682d64ef5d0f798a436b
Josep <chemabc@gmail.com>
2021-04-27 19:24:11 +0200

    cambio nombres archivos iterativo

--- src/Iterativo/Maze_Iterativo.cpp
+++ src/Iterativo/Maze_Iterativo.cpp
@@ -1,117 +1,70 @@
 //======================================================================
-// Maze_Iterativo.cpp
+// mainMaze_Iterativo.cpp
 //
-// Pile implementation and Visit function
+// C implementation of an iterative maze generation algorithm
 //
+// History:
+// 2021.04.19
+// Celia Ramos, Josep Maria Barberá, Gonzalo Quirós - created
+// 2021.04.21
+// Celia Ramos, Josep Maria Barberá, Gonzalo Quirós - First version
+// 2021.04.22
+// Josep Maria Barberá - Pile implementation
+// 2021.04.23 - Final version
 //======================================================================
+
 
 #include "Maze_Iterativo.hpp"
 
+
 //----VARIABLES GLOBALES------------------------------------------------
-extern int columnas,filas, vect[4];
-extern long int k;
+int columnas,filas, vect[4]={0,0,0,0};
+long int k=0;
 
-//----FUNCIONES DE LA PILA----------------------------------------------
-struct Nodo{
-    int x;
-    int y;
-    int iter;
-    bool estado;
-    int v[4];
-    Nodo *siguiente;
-};
-
-void agregarPila(Nodo *&pila, int pos_x, int pos_y, int i, bool estate, 
-                int dir[]){
-
-    Nodo *nuevo_nodo = (Nodo*)malloc(sizeof(Nodo));
-
-    nuevo_nodo -> x = pos_x;
-    nuevo_nodo -> y = pos_y;
-    nuevo_nodo -> iter = i;
-    nuevo_nodo -> estado = estate;
-
-    for(int j=0; j<4; j++) nuevo_nodo -> v[j]=dir[j];
-
-    nuevo_nodo -> siguiente = pila;
-    pila = nuevo_nodo;
-}
-
-void sacarPila(Nodo *&pila,int *pos_x, int *pos_y){
-    Nodo *aux = pila;
-    *pos_x = aux -> x;
-    *pos_y = aux -> y;
-    pila = aux -> siguiente;
-    free (aux);  
-}
-
-//----FUNCIONES QUE RECORREN LA GRID------------------------------------
-int IsInBounds(int x, int y){
-    // Devuelve "true" si x2 e y2 estan dentro de los límites.
-
-    if (x < 1 || x > (filas-2)) return false;
-    if (y < 1 || y > (columnas-2)) return false;
-
-    return true;
-}
-
-int Visit(int x, int y, char **grid, Nodo *pila){
+int main(){
     int i=0;
-    while(pila != NULL){
-        if(pila->estado){
-            if(pila->iter < 3){
-                ;
-            }
-            else if(pila != NULL){
-                sacarPila(pila,&x,&y);
-                continue;
-            }
-        }else{  
-            grid[x][y]=' ';
-            // Crea un vector local con las 4 direcciones 
-            //y cambia aleatoriamente su orden.
-            int dirs[4] = {ARRIBA, DERECHA, ABAJO, IZQUIERDA};
-
-            for (int i=0; i<4; ++i){
-                int r = rand()%4;   // Se elige una componente al azar.
-                int temp = dirs[r]; 
-                dirs[r] = dirs[i];  // Se intercambia por la componente i-ésima.
-                dirs[i] = temp;
-            }
-            pila->estado = true;
-            for(i=0;i<4;i++) pila->v[i]=dirs[i];
-        }
-        // Recorre cada dirección e intenta visitarla.
-        for (i=pila->iter; i<4; ++i){
-            // dx,dy are offsets from current location. Set them based
-            // on the next direction I wish to try.
-            int dx=0, dy=0;
-            switch (pila->v[i]){
-                case ARRIBA: dx = -1; break;
-                case ABAJO: dx = 1; break;
-                case DERECHA: dy = 1; break;
-                case IZQUIERDA: dy = -1; break;
-            }
-            // Find the (x,y) coordinates of the grid cell 2 spots
-            // away in the given direction.
-            int x2 = x + (dx*2);
-            int y2 = y + (dy*2);
-            // printf("antes(%u,%u)\n",x,y);
-            if (IsInBounds(x2,y2)){
-                if (grid[x2][y2] == '#'){
-                    // (x2,y2) has not been visited yet... knock down the
-                    // wall between my current position and that position
-                    grid[x2-dx][y2-dy] = ' ';
-                    x=x2;
-                    y=y2;
-                    pila->iter = i;
-                    agregarPila(pila,x,y,i=0,false,vect);
-                    break;
-                }
-            }
-        }
-        pila->iter = i;
-        k++;
+    
+    // Se pide al usuario el tamaño del laberinto.
+    pedir();
+    
+    Arreglar_2D();
+    
+    char **grid = NULL;
+    // Se reserva memoria para el vector de vectores dinámicos (del tamaño de "filas").
+    grid = (char **) malloc (filas * sizeof(char *)); 
+    if (grid == NULL) {
+        printf("No se pudo reservar memoria\n");
     }
-    return k;
-}   
+    // Se reserva memoria para cada fila (del tamaño de "columnas").
+    for (i = 0; i < filas; ++i) {
+        grid[i] = (char *) malloc (columnas * sizeof(char));
+        if (grid[i] == NULL) {
+            printf("No se pudo reservar memoria\n");
+        }
+    }
+    
+    srand(time(0));
+
+    // Hacemos la matriz
+    SetGrid(grid);
+
+    // Hacemos la pila
+    Nodo *pila = NULL;
+    agregarPila(pila,1,1,0,false,vect);
+
+    //Empieza la iteracion
+    Visit(1,1,grid,pila);
+
+    //Mostramos por pantalla
+    PrintGrid(grid);
+    
+    // Se libera memoria para cada fila.
+    for (i = 0; i < filas; ++i) {
+    	free(grid[i]);
+    }
+
+  	//Se libera memoria para el vector de vectores ("primera columna")
+    free(grid);
+
+    return 0;
+}
---
+++ src/Iterativo/Visit_Iterativo.cpp
@@ -0,0 +1,117 @@
+//======================================================================
+// Maze_Iterativo.cpp
+//
+// Pile implementation and Visit function
+//
+//======================================================================
+
+#include "Maze_Iterativo.hpp"
+
+//----VARIABLES GLOBALES------------------------------------------------
+extern int columnas,filas, vect[4];
+extern long int k;
+
+//----FUNCIONES DE LA PILA----------------------------------------------
+struct Nodo{
+    int x;
+    int y;
+    int iter;
+    bool estado;
+    int v[4];
+    Nodo *siguiente;
+};
+
+void agregarPila(Nodo *&pila, int pos_x, int pos_y, int i, bool estate, 
+                int dir[]){
+
+    Nodo *nuevo_nodo = (Nodo*)malloc(sizeof(Nodo));
+
+    nuevo_nodo -> x = pos_x;
+    nuevo_nodo -> y = pos_y;
+    nuevo_nodo -> iter = i;
+    nuevo_nodo -> estado = estate;
+
+    for(int j=0; j<4; j++) nuevo_nodo -> v[j]=dir[j];
+
+    nuevo_nodo -> siguiente = pila;
+    pila = nuevo_nodo;
+}
+
+void sacarPila(Nodo *&pila,int *pos_x, int *pos_y){
+    Nodo *aux = pila;
+    *pos_x = aux -> x;
+    *pos_y = aux -> y;
+    pila = aux -> siguiente;
+    free (aux);  
+}
+
+//----FUNCIONES QUE RECORREN LA GRID------------------------------------
+int IsInBounds(int x, int y){
+    // Devuelve "true" si x2 e y2 estan dentro de los límites.
+
+    if (x < 1 || x > (filas-2)) return false;
+    if (y < 1 || y > (columnas-2)) return false;
+
+    return true;
+}
+
+int Visit(int x, int y, char **grid, Nodo *pila){
+    int i=0;
+    while(pila != NULL){
+        if(pila->estado){
+            if(pila->iter < 3){
+                ;
+            }
+            else if(pila != NULL){
+                sacarPila(pila,&x,&y);
+                continue;
+            }
+        }else{  
+            grid[x][y]=' ';
+            // Crea un vector local con las 4 direcciones 
+            //y cambia aleatoriamente su orden.
+            int dirs[4] = {ARRIBA, DERECHA, ABAJO, IZQUIERDA};
+
+            for (int i=0; i<4; ++i){
+                int r = rand()%4;   // Se elige una componente al azar.
+                int temp = dirs[r]; 
+                dirs[r] = dirs[i];  // Se intercambia por la componente i-ésima.
+                dirs[i] = temp;
+            }
+            pila->estado = true;
+            for(i=0;i<4;i++) pila->v[i]=dirs[i];
+        }
+        // Recorre cada dirección e intenta visitarla.
+        for (i=pila->iter; i<4; ++i){
+            // dx,dy are offsets from current location. Set them based
+            // on the next direction I wish to try.
+            int dx=0, dy=0;
+            switch (pila->v[i]){
+                case ARRIBA: dx = -1; break;
+                case ABAJO: dx = 1; break;
+                case DERECHA: dy = 1; break;
+                case IZQUIERDA: dy = -1; break;
+            }
+            // Find the (x,y) coordinates of the grid cell 2 spots
+            // away in the given direction.
+            int x2 = x + (dx*2);
+            int y2 = y + (dy*2);
+            // printf("antes(%u,%u)\n",x,y);
+            if (IsInBounds(x2,y2)){
+                if (grid[x2][y2] == '#'){
+                    // (x2,y2) has not been visited yet... knock down the
+                    // wall between my current position and that position
+                    grid[x2-dx][y2-dy] = ' ';
+                    x=x2;
+                    y=y2;
+                    pila->iter = i;
+                    agregarPila(pila,x,y,i=0,false,vect);
+                    break;
+                }
+            }
+        }
+        pila->iter = i;
+        k++;
+    }
+    return k;
+}   
